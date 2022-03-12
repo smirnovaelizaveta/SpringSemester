4ee2778808757888cf94fbb53917e0dd33aee071
smirnovaelizaveta52 <smirnovaelizaveta52@gmail.com>
2022-03-12 20:45:12 +0300

    initial

--- src/main/java/ru/otus/spring/rest/BookController.java
+++ src/main/java/ru/otus/spring/rest/BookController.java
@@ -3,7 +3,6 @@ package ru.otus.spring.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import ru.otus.spring.bookService.AuthorService;
 import ru.otus.spring.bookService.BookGenreService;
