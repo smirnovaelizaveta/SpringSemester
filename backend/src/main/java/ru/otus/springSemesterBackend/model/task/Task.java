072346feca7a6f55bc90d0d318703669cb2d86fb
smirnovaelizaveta52 <smirnovaelizaveta52@gmail.com>
2022-04-08 22:30:56 +0300

    refactoring

--- backend/src/main/java/ru/otus/springSemesterBackend/model/task/Task.java
+++ backend/src/main/java/ru/otus/springSemesterBackend/model/task/Task.java
@@ -14,6 +14,7 @@ import java.io.File;
 public class Task {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String description;
